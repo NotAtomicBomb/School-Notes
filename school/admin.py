ed6437dd3682a7c947e2409357686795a06536a5
Jax <jaxdrummond9@gmail.com>
2022-10-07 15:32:13 -0400

    Removed Registrations for Choice and Question

--- school/admin.py
+++ school/admin.py
@@ -3,7 +3,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(Course)
 admin.site.register(Subject)
